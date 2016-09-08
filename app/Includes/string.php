5a62770e1efcbf0d6244797719cc8b612e559d29
sefsinalas <sefsinalas@gmail.com>
2016-09-08 00:16:23 -0300

    agrego algunas opciones a los campos string y text

--- app/Includes/string.php
+++ app/Includes/string.php
@@ -2,4 +2,6 @@
 	use App\Models\DogAdmin\Utils;
 
 	$properties['name'] = (empty($field->name)) ? Utils::decamelize($field->title) : $field->name;
+	$properties['disabled'] = (empty($field->disabled) || $field->disabled == false) ? '' : 'disabled';
+	$properties['hasPrefix'] = (empty($field->prefix)) ? '0' : '1';
 ?>
--- app/Includes/text.php
+++ app/Includes/text.php
@@ -2,4 +2,5 @@
 	use App\Models\DogAdmin\Utils;
 
 	$properties['name'] = (empty($field->name)) ? Utils::decamelize($field->title) : $field->name;
+	$properties['disabled'] = (empty($field->disabled) || $field->disabled == false) ? '' : 'disabled';
 ?>
